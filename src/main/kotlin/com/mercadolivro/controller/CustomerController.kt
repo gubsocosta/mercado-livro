e661ebeb3198126e0bf2635890eac0d6b404372a
Gabriel Costa <gubsocosta@gmail.com>
2021-10-13 14:53:36 -0300

    refactor: adjust customer creation

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -9,14 +9,22 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("customers")
 class CustomerController {
 
+    val customers = mutableListOf<CustomerModel>()
+
     @GetMapping
-    fun getCustomer(): CustomerModel {
-        return CustomerModel("1", "Gabriel", "gabriel@mail.com")
+    fun list(): List<CustomerModel> {
+        return customers
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody customer: PostCustomerRequest) {
-        println(customer)
+        val id = if (customers.isEmpty()) {
+            "1"
+        } else {
+            customers.last().id.toInt() + 1
+        }.toString()
+
+        customers.add(CustomerModel(id, customer.name, customer.email))
     }
 }
